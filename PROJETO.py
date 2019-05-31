e5e6423503364fed99b5211090ad0d76313d71bf
Alysson-UFC <51142101+Alysson-UFC@users.noreply.github.com>
2019-05-31 19:43:41 -0300

    Add files via upload
    Fiz duas funções por hora cara, uma é pro game abrir e a outra (no caso a segunda que aparece) seria para percorrer a matriz e fazer uma estrutura de decisão dizendo se a posição lida for '1', coloca a peça de dama que equivale ao do jogador1 e se a posição lida for '2', colocar a peça da dama que equivale ao do jogador2. Essa seria para colocar as peças nos lugares antes de iniciar o jogo, mas não terminei pq estou vendo como faço para percorrer a matriz sem o for ficar infinito, além de não saber como coloca as fotos das peças e nem a do tabuleiro.

--- PROJETO.py
+++ PROJETO.py
@@ -1,43 +1,84 @@
-from typing import Tuple
-
 import pygame
+import self as self
 from pygame.locals import*
 
-azul_jogador1=(25, 25, 112)                         #azul da meia noite #191970 --- PEÇA NORMAL
-azul_jogador1_dama=(72, 118, 255)                   #azul real 1 	#4876FF --- PEÇA QUE VIROU DAMA
-vermelho_jogador2=(205, 0, 0)                       #vermelho 2 #CD0000 --- PEÇA NORMAL
-vermelho_jogador2_dama=(139, 0, 0)                  #Vermelho 4 (darkred) #8B0000 --- PEÇA QUE VIROU DAMA
+azul_jogador1 = (25, 25, 112)                         #azul da meia noite #191970 --- PEÇA NORMAL
+azul_jogador1_dama = (72, 118, 255)                   #azul real 1 	#4876FF --- PEÇA QUE VIROU DAMA
+vermelho_jogador2 = (205, 0, 0)                       #vermelho 2 #CD0000 --- PEÇA NORMAL
+vermelho_jogador2_dama = (139, 0, 0)                  #Vermelho 4 (darkred) #8B0000 --- PEÇA QUE VIROU DAMA
+branco = (255, 255, 255)
+cor_de_chocolate = (255, 127, 36)                     #chocolate 1 #FF7F24 --- Cor de fundo do jogo onde vai ficar o tabuleiro
 
-cor_de_chocolate=(255, 127, 36)  #chocolate 1 #FF7F24 --- Cor de fundo do jogo onde vai ficar o tabuleiro
-fundo = cor_de_chocolate
+
+
 
 pygame.init()
 
-
-
-
-
-
 largura_tela = 800
 altura_tela = 600
-resolucao=[largura_tela, altura_tela]
+resolucao = [largura_tela, altura_tela]
+pos_x = 400
+pos_y = 300
 
-tela = pygame.display.set_mode((resolucao))
+jogador1 = '1'
+jogador2 = '2'
+
+fundo = cor_de_chocolate
+
+'''matriz_tabuleiro = [[0, 1, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 0, 1],
+                    [0, 0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0],
+                    [2, 0, 2, 0, 2, 0, 2, 0],
+                    [0, 2, 0, 2, 0, 2, 0, 2],
+                    [2, 0, 2, 0, 2, 0, 2, 0]]'''
+
+
+
+
+tela = pygame.display.set_mode((largura_tela, altura_tela))
 fundo_do_jogo = pygame.display.set_mode((resolucao))
 pygame.display.set_caption(("Jogo De Damas"))
 
+tamanho_tela_tabuleiro = 200
 
 
-#pygame.display.update()
 
-abrir = True
+def jogo():
+    abrir = True                                    # Se o abrir == False de vocês tiver dando erro coloca pygame.quit()
+    while abrir:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                    abrir = False
+            #print(events)
+
+            fundo_do_jogo.fill(fundo)
+        fundo_do_tabuleiro = pygame.draw.rect(tela, branco, [pos_x, pos_y, tamanho_tela_tabuleiro, tamanho_tela_tabuleiro])
+
+        pygame.display.update()
 
 
-while abrir:
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            abrir = False
-        print(events)
 
-fundo_do_jogo.fill(cor_de_chocolate)
 
+
+
+def colocando_as_peças():                                   #Aqui o tabuleiro inicia com as peças colocadas de forma padrão antes do jogo começar
+        matriz_tabuleiro = [[0, 1, 0, 1, 0, 1, 0, 1],
+                            [1, 0, 1, 0, 1, 0, 1, 0],
+                            [0, 1, 0, 1, 0, 1, 0, 1],
+                            [0, 0, 0, 0, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [2, 0, 2, 0, 2, 0, 2, 0],
+                            [0, 2, 0, 2, 0, 2, 0, 2],
+                            [2, 0, 2, 0, 2, 0, 2, 0]]
+
+        '''for pos_linha in range(len(matriz_tabuleiro)):
+            for pos_coluna in range(len(matriz_tabuleiro[0])):
+                if'''
+
+
+
+colocando_as_peças()
+jogo()
+pygame.quit()
