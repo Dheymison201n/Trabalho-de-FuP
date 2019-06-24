92a7171527ee00bcc31306d16722dea9d49a76a9
Dheymison201n <39660020+Dheymison201n@users.noreply.github.com>
2019-06-23 22:41:19 -0300

    Update Python em python.py

--- Python em python.py
+++ Python em python.py
@@ -1,12 +1,19 @@
 import pygame, random
-from pygame import *
+from pygame.locals import *
 
-imagem_de_fundo = pygame.image.load('Background_do_jogo_800x600.jpg')
+imagem_de_fundo = pygame.image.load('imagem.jpg')
 
 def musica_jogatina():
     # Música de fundo do jogo durante a jogatina
     pygame.mixer.music.load('musica.wav')
     pygame.mixer.music.play()
+
+# Nessa função a comida vai aparecer em algum lugar randômico sem está fora da linha, isto é, "10 por 10"
+def comida_do_rato():
+    x = random.randint(0, 590)
+    y = random.randint(0, 590)
+
+    return (x // 10 * 10, y // 10 * 10)
 
 
 def comer_a_comida(colisao_cobra, colisao_comida):
@@ -17,153 +24,121 @@ def comer_a_comida(colisao_cobra, colisao_comida):
 volume = pygame.mixer.music.get_volume
 print(volume)
 
+cima = 0
+direita = 1
+baixo = 2
+esquerda = 3
 
 pygame.init()
-
-tela = pygame.display.set_mode((800, 600))
+tela = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Python em Python')
-
-pontuacao = 0
-contador = 0
 
 #chama a função que inicia a música do jogo
 musica_jogatina()
 
-#valores das direções que a cobra vai andar
-para_cima = 0
-para_direita = 1
-para_baixo = 2
-para_esquerda = 3
-
-#Corpo da cobra
 cobra = [(200, 200), (210, 200), (220, 200)]
+
 pele_da_cobra = pygame.Surface((10, 10))
 pele_da_cobra.fill((255, 255, 0))
 cabeca_da_cobra = pygame.Surface((10, 10))
 cabeca_da_cobra.fill((0, 0, 255))
 
-
-# Nessa função a comida vai aparecer em algum lugar randômico sem está fora da linha, isto é, "10 por 10"
-def comida_do_rato():
-    x = random.randint(0, 790)
-    y = random.randint(0, 590)
-
-    return (x//10 * 10, y//10 * 10)
-
-
-
-#criando a comida
-rato_comer = comida_do_rato()
+rato_pos = comida_do_rato()
 rato = pygame.Surface((10, 10))
-rato.fill((255, 70, 30))
+rato.fill((255, 0, 0))
 
-#direção inicial da cobra(A cabeça está do lado esquedo, logo o corpo cresce para o lado direito)
-direção = para_esquerda
+direcao_da_cobra = esquerda
 
-movimentação_da_cobra = pygame.time.Clock()
+tempo = pygame.time.Clock()
 
+fonte = pygame.font.Font('freesansbold.ttf', 18)
 
-inicio_do_jogo = True
-while inicio_do_jogo:
-    movimentação_da_cobra.tick(12) #aqui é a velocidade que a cobra vai andar
-    for event in pygame.event.get(): #Faz tem a opção de fechar o jogo
+pontuacao = 0
+
+cont = 10
+
+level_v = 1
+
+level = 0
+
+game_over = False
+
+tela.blit(imagem_de_fundo, (0, 0))
+
+while not game_over:
+
+    tempo.tick(cont)
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-
-    tela.blit(imagem_de_fundo, (0, 0))
-
-    # Aqui faz o corpo andar junto com a cabeça
-    for k in range(len(cobra) -1, 0, -1):
-        cobra[k] = (cobra[k-1][0], cobra[k-1][1])
-
-    if direção == para_cima:
-       cobra[0] = (cobra[0][0], cobra[0][1] - 10)
-    if direção == para_baixo:
-        cobra[0] = (cobra[0][0], cobra[0][1] + 10)
-    if direção == para_direita:
-        cobra[0] = (cobra[0][0] + 10, cobra[0][1])
-    if direção == para_esquerda:
-        cobra[0] = [cobra[0][0] - 10, cobra[0][1]]
+            exit()
 
 
-#Nessa parte a variável direção recebe a "direção"  que a cobra vai de acordo com a tecla clicada, no caso as setas
-    if event.type == KEYDOWN:
-        if event.key == K_UP:
-            direção = para_cima
-        if event.key == K_DOWN:
-            direção = para_baixo
-        if event.key == K_LEFT:
-            direção = para_esquerda
-        if event.key == K_RIGHT:
-            direção = para_direita
+        if event.type == KEYDOWN:
+            if event.key == K_UP and direcao_da_cobra != baixo:
+                direcao_da_cobra = cima
+            if event.key == K_DOWN and direcao_da_cobra != cima:
+                direcao_da_cobra = baixo
+            if event.key == K_LEFT and direcao_da_cobra != esquerda:
+                direcao_da_cobra = esquerda
+            if event.key == K_RIGHT and direcao_da_cobra != direita:
+                direcao_da_cobra = direita
 
-    """for k in range(len(cobra) -1, 0, -1):
-        cobra[k] = (cobra[k-1][0], cobra[k-1][1])"""
-
-
-    if comer_a_comida(cobra[0], rato_comer):
-        rato_comer = comida_do_rato()
-        pontuacao = pontuacao + 25
+    if comer_a_comida(cobra[0], rato_pos):
+        rato_pos = comida_do_rato()
         cobra.append((0, 0))
-        contador = contador + 1
+        pontuacao = pontuacao + 25
+        level = level + 1
 
-    """Nessa parte essa estrutura de decisão vai fazer com que a cobra morra se tentar sair da 
-        tela de jogo"""
-
-    if cobra[0][0] == 800 or cobra[0][1] == 600 or cobra[0][0] < 0 or cobra[0][1] < 0:
-        inicio_do_jogo = False
+    if cobra[0][0] == 600 or cobra[0][1] == 600 or cobra[0][0] < 0 or cobra[0][1] < 0:
+        game_over = True
         break
 
 
-    #tela.fill((255, 255, 255))
-    tela.blit(rato, rato_comer)
-    tela.blit(cabeca_da_cobra, cobra[0])
-    for i in range(1, len(cobra)):
-        tela.blit(pele_da_cobra, cobra[i])
+    for i in range(1, len(cobra) - 1):
+        if cobra[0][0] == cobra[i][0] and cobra[0][1] == cobra[i][1]:
+            game_over = True
+            break
 
-    #for pos in cobra:
-    #tela.blit(pele_da_cobra, pos)
-    pygame.font
+    if game_over:
+        break
 
+    for i in range(len(cobra) - 1, 0, -1):
+        cobra[i] = (cobra[i - 1][0], cobra[i - 1][1])
 
+    if direcao_da_cobra == cima:
+        cobra[0] = (cobra[0][0], cobra[0][1] - 10)
+    if direcao_da_cobra == baixo:
+        cobra[0] = (cobra[0][0], cobra[0][1] + 10)
+    if direcao_da_cobra == direita:
+        cobra[0] = (cobra[0][0] + 10, cobra[0][1])
+    if direcao_da_cobra == esquerda:
+        cobra[0] = (cobra[0][0] - 10, cobra[0][1])
 
-    # Nessa parte faz aparecer a pontuação do jogador
-    pygame.font.init()
-    font = pygame.font.get_default_font() # Fonte padrão
-    mostrar_pont = pygame.font.SysFont(font, 40)  #tamanho 40
-    texto_pontuacao = mostrar_pont.render('Pontuação: %s' % (pontuacao), True, (255, 200, 70))
-    tela.blit(texto_pontuacao, (550, 10))
+    tela.fill((0, 0, 0))
+    tela.blit(rato, rato_pos)
+
+    for x in range(0, 600, 10):
+        pygame.draw.line(tela, (40, 40, 40), (x, 0), (x, 600))
+    for y in range(0, 600, 10):
+        pygame.draw.line(tela, (40, 40, 40), (0, y), (600, y))
+
+    pontuacao_fonte = fonte.render('Pontuação: %s' % (pontuacao), True, (255, 255, 255))
+    pontuacao_r = pontuacao_fonte.get_rect()
+    pontuacao_r.topleft = (200 - 120, 10)
+    tela.blit(pontuacao_fonte, pontuacao_r)
+
+    pontuacao_fonte = fonte.render('level: %s' % (level_v), True, (255, 255, 255))
+    pontuacao_r = pontuacao_fonte.get_rect()
+    pontuacao_r.topleft = (500 - 120, 10)
+    tela.blit(pontuacao_fonte, pontuacao_r)
+
+    if level == 4:
+        cont = cont + 5
+        level = 0
+        level_v = level_v + 1
+
+    for pos in cobra:
+        tela.blit(pele_da_cobra, pos)
 
     pygame.display.update()
-
-pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""for k in range(len(cobra) -1, 0, -1):
-       cobra[k] = (cobra[k-1][0], cobra[k-1][1])"""
